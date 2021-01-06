7621f7c58391c87298749dc07a26c249fe161257
junilhwang <tjsdlf4261@naver.com>
2021-01-06 10:50:42 +0900

    feat: 타임존 작업 중 (2)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+require('dayjs/locale/ko');
+
 const axios = require("axios");
-const date = new Date();
-const y = date.getFullYear();
-const m = `0${date.getMonth() + 1}`.substr(-2);
-const d = `0${date.getDate()}`.substr(-2);
+const dayjs = require("dayjs");
+dayjs.locale('ko');
 
 axios.post(`https://api.github.com/repos/${process.env.REPO}/issues`, {
-  title: `${y} ${m} ${d}`,
-  body: `${date.toLocaleTimeString()}`
+  title: dayjs().format("YYYY MM DD"),
+  body: dayjs().format("YYYY-MM-DD HH:mm:ss")
 }, {
   headers: {
     authorization: `Bearer ${process.env.TOKEN}`
