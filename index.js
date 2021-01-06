379be63f3849072fa1251d227081f22487f7f724
junilhwang <tjsdlf4261@naver.com>
2021-01-06 10:42:29 +0900

    feat: 타임존 작업 중

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const d = `0${date.getDate()}`.substr(-2);
 
 axios.post(`https://api.github.com/repos/${process.env.REPO}/issues`, {
   title: `${y} ${m} ${d}`,
-  body: `${date.toISOString()}`
+  body: `${date.toLocaleTimeString()}`
 }, {
   headers: {
     authorization: `Bearer ${process.env.TOKEN}`
