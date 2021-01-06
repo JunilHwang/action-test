d3fdf13d79dfe6cb2f82de76675468c3e876bd5c
junilhwang <tjsdlf4261@naver.com>
2021-01-06 11:37:20 +0900

    chore: 불필요한 파일 삭제

--- date.js
+++
@@ -1,8 +0,0 @@
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
-const dayjs = require("dayjs");
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.tz.setDefault("Asia/Seoul");
-
-console.log(dayjs().format("YYYY-MM-DD HH:mm:ss"));
