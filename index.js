3f97885448cd5cd083deb5029105f396f66d8080
junilhwang <tjsdlf4261@naver.com>
2021-01-06 10:28:39 +0900

    feat: nodejs로 issue를 추가하는 작업 완료

---
+++ index.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const date = new Date();
+const y = date.getFullYear();
+const m = `0${date.getMonth() + 1}`.substr(-2);
+const d = `0${date.getDate()}`.substr(-2);
+
+axios.post(`https://api.github.com/repos/${process.env.REPO}/issues`, {
+  title: `${y} ${m} ${d}`,
+  body: ''
+}, {
+  headers: {
+    authorization: `Bearer ${process.env.TOKEN}`
+  }
+}).then(({ data }) => console.log(data));
